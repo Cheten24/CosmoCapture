3201154e8d5a444b3c68338521d4b353ba83015b
Navteg Singh Bains <22103967@students.latrobe.edu.au>
2026-05-04 15:32:44 +1000

    Ignore node modules and environment files

--- cse3cap-rsdcc-main/frontend/src/components/safety/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Safety components for telescope safety scheduling system
- */
-
-export { default as SafetyStatusDisplay } from '../SafetyStatusDisplay';
-export { default as ControlLockOverlay } from '../ControlLockOverlay';
-
-// Re-export types for convenience
-export type { SafetyStatusType, SafetyStatus, WeatherSafety, ViewingWindow } from '../../types/safety';
